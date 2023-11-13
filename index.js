54c9a95bb02063be8132e0567acacf046ec73e88
Shovon96 <fakhruddinshovon@gmail.com>
2023-11-13 22:34:24 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ require('dotenv').config()
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-
 // middleWare
 app.use(cors())
 app.use(express.json())
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.riywk8u.mongodb.net/?retryWrites=true&w=majority`;
@@ -32,7 +30,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
