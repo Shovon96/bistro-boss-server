3f3cd5c06d55d4961ba35d8ed0bf61a075144014
Shovon96 <fakhruddinshovon@gmail.com>
2023-11-15 12:55:36 +0600

    carts post in db

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.riywk8u.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-phmeoes-shard-00-00.riywk8u.mongodb.net:27017,ac-phmeoes-shard-00-01.riywk8u.mongodb.net:27017,ac-phmeoes-shard-00-02.riywk8u.mongodb.net:27017/?ssl=true&replicaSet=atlas-dhaabf-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,21 +25,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
+console.log('connect with mongodb');
     // collections
     const menuCollection = client.db('bistroBoss').collection('menu')
     const reviewCollection = client.db('bistroBoss').collection('reviews')
+    const cartCollection = client.db('bistroBoss').collection('carts')
 
-    // menu apis
+    // menus data get apis
     app.get('/menu', async (req, res)=> {
         const result = await menuCollection.find().toArray()
         res.send(result)
     })
 
-    // reviews apis
+    // reviews data get apis
     app.get('/reviews', async (req, res)=> {
         const result = await reviewCollection.find().toArray()
         res.send(result)
+    })
+
+    // carts releted apis
+    app.post('/carts', async(req, res)=>{
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem)
+      res.send(result)
     })
 
 
