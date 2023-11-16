bdda763831abaab2114239f2ef5440145515e8c6
Shovon96 <fakhruddinshovon@gmail.com>
2023-11-17 01:34:41 +0600

    verify admin user

--- index.js
+++ index.js
@@ -102,14 +102,14 @@ async function run() {
       res.send({ admin })
     })
 
-    app.delete('/users/:id', async (req, res) => {
+    app.delete('/users/:id', verifyAdmin, verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
       res.send(result);
     })
 
-    app.patch('/users/admin/:id', async (req, res) => {
+    app.patch('/users/admin/:id',verifyAdmin, verifyToken, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) }
       const updatedDoc = {
